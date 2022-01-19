9a64b0d4aee42403046f8ec23681b55bcec0af9e
qinmingyuan <mingyuan0715@foxmail.com>
2022-01-19 23:41:08 +0800

    project init

---
+++ README.md
@@ -0,0 +1,13 @@
+# service
+
+## 准备
+
+`pnpm install`
+
+## 开发
+
+`pnpm run start`
+
+
+## Docker
+`docker-compose -f docker/docker-compose.node.yml build`
---
+++ index.js
@@ -0,0 +1,30 @@
+import Fastify from 'fastify'
+import { Marp } from '@marp-team/marp-core'
+
+
+const fastify = Fastify({
+  logger: true
+})
+const marp = new Marp({
+  markdown: {
+    html: true,
+    breaks: true
+  }
+})
+
+
+// 声明路由
+fastify.post('/marp', (request, reply) => {
+  const body = request.body
+  const { html, css, comments } = marp.render(body, { htmlAsArray: true })
+
+  reply.send({ html: html, css: css, comments: comments })
+})
+
+// 启动服务！
+fastify.listen(4000, (err, address) => {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+})
